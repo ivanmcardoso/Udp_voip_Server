fe91bcdd547291ec893f873504dd2ab117c3a162
ivanmcardoso <ivanmcardoso@hotmail.com>
2019-07-21 20:45:48 -0400

    Now its a AP

--- main/main.c
+++ main/main.c
@@ -17,11 +17,11 @@
 #include <driver/adc.h>
 #include "driver/i2s.h"
 
-#define SSID	"Katia Cardoso"
-#define PASSWORD	"jnc196809"
+#define SSID	"ESP32"
+#define PASSWORD	"121235RA"
 #define PORT	3333
 #define SAMPLE_RATE	8000
-#define BUFFER_MAX	8000
+#define BUFFER_MAX	500
 #define LED_GOTIP	GPIO_NUM_2
 size_t i2s_bytes_write = 0;
 
@@ -41,7 +41,7 @@ static void recv_all(int sock, void *vbuf, size_t size_buf)
     socklen_t socklen = sizeof(source_addr);
 
 	size_left = size_buf;
-	while(1)
+	while(size_left>0)
 	{
 		if((recv_size = recvfrom(sock, buf, size_left, flags, (struct sockaddr *)&source_addr, &socklen)) == -1)
 		{
@@ -54,6 +54,7 @@ static void recv_all(int sock, void *vbuf, size_t size_buf)
 			break;
 		}
 		if(recv_size > 0)i2s_write(0, buf, recv_size,&i2s_bytes_write,  portMAX_DELAY);
+		else if(recv_size<=0)printf("erro\n");
 		printf("size left = %d\n",size_left);
 		size_left -= recv_size;
 		buf += recv_size;
@@ -88,59 +89,48 @@ static void udp_server_task(void *pvParameters)
 
 }
 
-static void wifi_reconnect(){
-    do {
-        xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-        vTaskDelay(300/portTICK_PERIOD_MS);
-    }while(esp_wifi_connect() != ESP_OK);
-}
-
 static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
-    switch(event->event_id) {
-    case SYSTEM_EVENT_STA_START:
-        esp_wifi_connect();
-        break;
-    case SYSTEM_EVENT_STA_CONNECTED:
-        break;
-    case SYSTEM_EVENT_STA_GOT_IP:
-    	gpio_set_level(LED_GOTIP, 1);
-    	xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
-        xTaskCreatePinnedToCore(udp_server_task, "udp_server", 4096, NULL, 5, NULL,0);
-        break;
-    case SYSTEM_EVENT_STA_DISCONNECTED:
-       	gpio_set_level(LED_GOTIP, 0);
-       	wifi_reconnect();
+
+	switch(event->event_id){
+	case SYSTEM_EVENT_AP_STACONNECTED:
+		gpio_set_level(GPIO_NUM_2, 1);
+		xTaskCreatePinnedToCore(udp_server_task, "udp_server", 4096, NULL, 5, NULL,0);
+		break;
+    case SYSTEM_EVENT_AP_STADISCONNECTED:
+		gpio_set_level(GPIO_NUM_2, 0);
         break;
     default:
-        break;
-    }
-    return ESP_OK;
+    	break;
+	}
+	return ESP_OK;
 }
-
 
 void setup_wifi()
 {
-    s_wifi_event_group = xEventGroupCreate();
 
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
+	s_wifi_event_group = xEventGroupCreate();
 
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = SSID,
-            .password = PASSWORD
-        },
-    };
+	tcpip_adapter_init();
+	ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_start() );
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+	wifi_config_t wifi_config = {
+			.ap = {
+					.ssid = SSID,
+					.ssid_len = strlen(SSID),
+					.password = PASSWORD,
+					.max_connection = 4,
+					.authmode = WIFI_AUTH_WPA_WPA2_PSK
+			},
+	};
+
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+	ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP,&wifi_config));
+	ESP_ERROR_CHECK(esp_wifi_start());
 
 }
-
 static void i2s_setup(){
 
 	i2s_config_t i2s_config = {
@@ -149,8 +139,8 @@ static void i2s_setup(){
         .bits_per_sample = 16,
         .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
         .communication_format = I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB,
-        .dma_buf_count = 2,
-        .dma_buf_len = 1000,
+        .dma_buf_count = 3,
+        .dma_buf_len = 1024,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1
     };
     i2s_pin_config_t pin_config = {
